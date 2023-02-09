56cdf18a6d87fe7ec1125c820220f15b6e96f72f
Richard-Gaspar <richard.j.gaspar@gmail.com>
2023-02-09 10:46:05 +0100

    feat(events-section): cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,18 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HerocarouselComponent } from './hero-carousel/hero-carousel.component';
 import { HistoryComponent } from './history/history.component';
+import { EventsSectionComponent } from './events-section/events-section.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HerocarouselComponent, HistoryComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HerocarouselComponent,
+    HistoryComponent,
+    EventsSectionComponent,
+    CardComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
