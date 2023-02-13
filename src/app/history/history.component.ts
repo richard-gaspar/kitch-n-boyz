3497236d3e22a89107b1bd4ec62b7a882b1f4754
Richard-Gaspar <richard.j.gaspar@gmail.com>
2023-02-13 12:21:36 +0100

    style(history): change layout

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
-  styleUrls: ['./history.component.scss']
+  styleUrls: ['./history.component.scss'],
 })
 export class HistoryComponent {
+  photoFadeUp: boolean = false;
 
+  // input = document.querySelector('#photo-2') as HTMLInputElement | null;
+
+  @HostListener('document:scroll')
+  onScroll() {
+    if (document.documentElement.scrollTop > 1200) {
+      this.photoFadeUp = true;
+    }
+  }
 }
