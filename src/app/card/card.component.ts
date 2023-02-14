efb40da818fa05470e39c62262f9a26742866158
Richard-Gaspar <richard.j.gaspar@gmail.com>
2023-02-14 10:01:56 +0100

    feat(events-section): event cards design

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+  public events = [
+    {
+      title: 'Journey of Taste',
+      text: 'Indulge in our exquisite 6-course degustation menu, perfectly paired with fine wines for a truly elegant dining experience.',
+      price: 'NOK 2000',
+    },
 
+    {
+      title: 'Blossom into Spring',
+      text: 'Awaken your senses with our seasonal 10-course spring menu, crafted with fresh, vibrant flavors.',
+      price: 'NOK 2500',
+    },
+
+    {
+      title: 'Savor the Romance',
+      text: "Experience romance and fine dining with our Valentine's Day 4 course menu, crafted to delight the senses.",
+      price: 'NOK 1750',
+    },
+  ];
 }
--- src/app/events-section/events-section.component.ts
+++ src/app/events-section/events-section.component.ts
@@ -5,11 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './events-section.component.html',
   styleUrls: ['./events-section.component.scss'],
 })
-export class EventsSectionComponent {
-  events = [
-    {
-      title: '6 course degustive menu',
-      text: 'Savor each carefully crafted dish as it takes you on a journey through a symphony of flavors, textures, and aromas. Our menu, a harmonious blend of tradition and modernity, showcases the finest seasonal ingredients, carefully selected to create a truly unforgettable dining experience. Book your table now.',
-    },
-  ];
-}
+export class EventsSectionComponent {}
