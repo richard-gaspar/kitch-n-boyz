14fc7e42d61ee45519029e3424831bb652011006
Richard-Gaspar <richard.j.gaspar@gmail.com>
2023-02-15 14:23:46 +0100

    feat(gallery): Grid gallery

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
 })
 export class GalleryComponent {
-
+  photos = [
+    { url: 'assets/images/gallery/food-0.jpg' },
+    { url: 'assets/images/gallery/food-1.jpg' },
+    { url: 'assets/images/gallery/food-2.jpg' },
+    { url: 'assets/images/gallery/food-3.jpg' },
+    { url: 'assets/images/gallery/food-4.jpg' },
+    { url: 'assets/images/gallery/food-5.jpg' },
+    { url: 'assets/images/gallery/food-6.jpg' },
+    { url: 'assets/images/gallery/food-7.jpg' },
+    { url: 'assets/images/gallery/food-8.jpg' },
+    { url: 'assets/images/gallery/food-9.jpg' },
+    { url: 'assets/images/gallery/food-10.jpg' },
+    { url: 'assets/images/gallery/food-11.jpg' },
+    { url: 'assets/images/gallery/food-12.jpg' },
+  ];
 }
