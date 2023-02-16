2731d78db8fb64a392ee576a6d4918e4b8e55d39
Richard-Gaspar <richard.j.gaspar@gmail.com>
2023-02-16 10:12:35 +0100

    style():  change layouts

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent {
-
+  appetizerCourses = [
+    {
+      name: 'something',
+      ingredients: ['onion', 'caramell', 'pear'],
+    },
+    {
+      name: 'something2',
+      ingredients: ['oasdasdas', 'caramell', 'pear'],
+    },
+    {
+      name: 'something different',
+      ingredients: [
+        'onion',
+        'caraasdasdasell',
+        'peaasdasdassar',
+        'more something',
+      ],
+    },
+    {
+      name: 'something else',
+      ingredients: ['bread', 'turkey', 'tmato'],
+    },
+  ];
 }
