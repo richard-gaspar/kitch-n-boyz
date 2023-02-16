1f51ca35ca4149140100418da7eb18640113c81d
Richard-Gaspar <richard.j.gaspar@gmail.com>
2023-02-16 10:58:43 +0100

    feat(menu): design and content

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,25 +8,95 @@ import { Component } from '@angular/core';
 export class MenuComponent {
   appetizerCourses = [
     {
-      name: 'something',
-      ingredients: ['onion', 'caramell', 'pear'],
-    },
-    {
-      name: 'something2',
-      ingredients: ['oasdasdas', 'caramell', 'pear'],
-    },
-    {
-      name: 'something different',
+      name: 'Crispy Octopus',
       ingredients: [
-        'onion',
-        'caraasdasdasell',
-        'peaasdasdassar',
-        'more something',
+        'confit potatoes',
+        'pearl onions',
+        'blistered shishitos',
+        'cotija',
+        'creamy salsa verde',
+        'almond corn purée',
       ],
     },
     {
-      name: 'something else',
-      ingredients: ['bread', 'turkey', 'tmato'],
+      name: 'Poutine',
+      ingredients: ['tomato biscuit', ' marinated tomatoes', 'pesto purée'],
+    },
+    {
+      name: 'Roasted Beets',
+      ingredients: [
+        'beet gazpacho',
+        'strawberries',
+        'green goddess',
+        'whipped strawberry boursin',
+      ],
+    },
+    {
+      name: 'Garlic Noodles',
+      ingredients: ['mushrooms', 'scallions', 'parmesan'],
+    },
+  ];
+
+  entreeCourses = [
+    {
+      name: 'Shrimp & Lobster Roll',
+      ingredients: ['lemon aioli ', 'celery', 'chili flakes', 'green onions'],
+    },
+    {
+      name: 'Fettuccine',
+      ingredients: ['black beans', ' corn', 'romesco sauce'],
+    },
+    {
+      name: 'Blackened Salmon',
+      ingredients: [
+        'confit potatoes ',
+        'blistered shishitos',
+        'green seaweed',
+        'fermented salsify',
+      ],
+    },
+    {
+      name: 'Grilled Venison',
+      ingredients: [
+        'artichoke puree',
+        'blackberry sauce',
+        'caramellized carrot',
+      ],
+    },
+    {
+      name: 'Nordic Sunburst Salad',
+      ingredients: [
+        'dried cherries',
+        'cucumber',
+        'spicy cinnamon vinaigrette',
+        'gorgonzola cheese',
+        'organic mixed greens',
+      ],
+    },
+  ];
+
+  dessertCourses = [
+    {
+      name: 'Bondepiker',
+      ingredients: ['apple', 'cream', 'walnut crumble', 'caramell sauce'],
+    },
+    {
+      name: 'Success tart',
+      ingredients: [
+        'crunched almond',
+        'egg cream liqueur',
+        'chocolate',
+        'fermented orange',
+      ],
+    },
+    {
+      name: 'Sweet temptations',
+      ingredients: [
+        'butter cream ',
+        'strawberry chutney',
+        'youghurt',
+        'pistachio sponge',
+      ],
     },
   ];
 }
